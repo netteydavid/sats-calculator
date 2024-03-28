340d0ee2fe00e4b0b0485580b9c12f682693daa2
[David Nettey] <[davidnettey@protonmail.com]>
2024-03-28 18:01:03 -0500

    Added textboxes and calculations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let price = 0;
+    let amount = 0;
+    let value = 0;
+
+    function calculateValue(event){
+        if (price == null) return;
+        if (amount == null) return;
+        price = Number.parseFloat(price.toFixed(2));
+        var result = price * (amount / 100000000);
+        value = Number.parseFloat(result.toFixed(2));
+    }
+</script>
+
+<h1>Sats Calculator</h1>
+
+<h2>Price (USD)</h2>
+<input bind:value={price}
+ on:input={calculateValue}
+ type="number" 
+ step=".01" 
+ id="price_input" />
+<br />
+
+<h2>Bitcoin Amount (sats)</h2>
+<input bind:value={amount}
+ on:input={calculateValue}
+ type="number"
+  id="btc_amt_input" />
+<br />
+
+<h2>Value (USD)</h2>
+<input bind:value={value}
+ type="number" 
+ step=".01" 
+ id="value_input" />
